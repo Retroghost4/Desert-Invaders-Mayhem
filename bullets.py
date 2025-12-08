d5db283876311f301caebd54c00485ada1785e41
Kaiya <retrospectart4@gmail.com>
2025-12-08 17:02:24 -0600

    fixed bulkets

--- bullets.py
+++ bullets.py
@@ -7,22 +7,22 @@ class Bullet(pygame.sprite.Sprite):
     self.x = pos[0]
     self.y = pos[1]
 
-img_path = '{side}-bullet.png'
-self.image = pygame.image.load(img_path)
-self.image = pygame.transform.scale(self.image, (size, size))
-self.rect = self.image.get_rect(topleft = pos)
-self.mask = pygame.mask.from_surface(self.image)
+    img_path = '{side}-bullet.png'
+    self.image = pygame.image.load(img_path)
+    self.image = pygame.transform.scale(self.image, (size, size))
+    self.rect = self.image.get_rect(topleft = pos)
+    self.mask = pygame.mask.from_surface(self.image)
 
-if side == 'Enemy':
-  self.move_speed = BULLET_SPEED
-elif side == 'player':
-  self.move_speed = (- BULLET_SPEED)
+    if side == 'Enemy':
+     self.move_speed = BULLET_SPEED
+    elif side == 'player':
+     self.move_speed = (- BULLET_SPEED)
 
-def _move_bullet(self):
-  self.rect.y += self.move_speed
+  def _move_bullet(self):
+    self.rect.y += self.move_speed
 
-def update(self):
-  self._move_bullet()
-  self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
-  if self.rect.bottom <= 0 or self.rect.top >= HEIGHT:
-    self.kill()
+  def update(self):
+    self._move_bullet()
+    self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+    if self.rect.bottom <= 0 or self.rect.top >= HEIGHT:
+     self.kill()
