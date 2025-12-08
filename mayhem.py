6f538575b60f4081fff1f4e9af8b70ac7917460c
Kaiya <retrospectart4@gmail.com>
2025-12-08 16:48:38 -0600

    fixed mayhem

--- mayhem.py
+++ mayhem.py
@@ -1,6 +1,6 @@
 import pygame
 from settings import PLAYER_SPEED, BULLET_SIZE
-from bullet import Bullet
+from bullets import Bullet
 
 class Mayhem(pygame.sprite.Sprite):
   def __init__(self, pos, size):
@@ -16,21 +16,21 @@ class Mayhem(pygame.sprite.Sprite):
     self.life = 3
     self.player_bullets = pygame.sprite.Group()
 
-def move_left(self): 
-  self.rect.x -= self.ship_speed
+  def move_left(self): 
+    self.rect.x -= self.ship_speed
 
-def move_up(self): 
-  self.rect.y -= self.ship_speed
+  def move_up(self): 
+    self.rect.y -= self.ship_speed
 
-def move_right(self): 
-  self.rect.x += self.ship_speed
+  def move_right(self): 
+    self.rect.x += self.ship_speed
 
-def move_bottom(self): 
-  self.rect.y += self.ship_speed
+  def move_bottom(self): 
+    self.rect.y += self.ship_speed
 
-def _shoot(self):
-  specific_pos = (self.rect.centerx -(BULLET_SIZE // 2), self.rect.y)
-  self.player_bullets.add(Bullet(specific_pos, BULLET_SIZE, "player"))
+  def _shoot(self):
+    specific_pos = (self.rect.centerx -(BULLET_SIZE // 2), self.rect.y)
+    self.player_bullets.add(Bullet(specific_pos, BULLET_SIZE, "player"))
 
-def update(self): 
-  self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+  def update(self): 
+    self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
